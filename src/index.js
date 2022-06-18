7dd207913a81e486e09e3daaa1b672b5ba655fa1
AcChrome <ac.chan@live.com>
2022-06-17 22:38:09 -0700

    main content added

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import Header from "./Header";
+import MainContent from "./MainContent";
 
 function Page() {
   return (
     <div>
       <Header />
+      <MainContent />
     </div>
   );
 }
