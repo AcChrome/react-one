5005d777c0b72d037eca7b25fbaeb23a2d52f804
AcChrome <ac.chan@live.com>
2022-06-17 22:36:41 -0700

    added header

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom";
+import Header from "./Header";
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
+function Page() {
+  return (
+    <div>
+      <Header />
+    </div>
+  );
+}
+
+ReactDOM.render(<Page />, document.getElementById("root"));
