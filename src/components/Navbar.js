ed0e4f897dd0d4f5481e4e025f41a40b49853391
AcChrome <ac.chan@live.com>
2022-07-05 23:28:49 -0700

    fixed up styling for Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,11 @@
 import React from "react";
+import Icon from "../favicon.ico";
+import "../style.css";
 
 export default function Navbar() {
   return (
     <nav>
-      <img src="" className="nav--icon" />
+      <img src={Icon} className="nav--icon" />
       <h3 className="nav--logo_text">ReactFacts</h3>
       <h4 className="nav--title">React Course - Project 1</h4>
     </nav>
--- src/index.js
+++ src/index.js
@@ -1,20 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import Header from "./Header";
-import MainContent from "./components/Main";
-import Footer from "./Footer";
 import App from "./App";
-
-// function Page() {
-//   return (
-//     <div>
-//       <Header />
-//       <MainContent />
-//       <Footer />
-//     </div>
-//   );
-// }
-
-// ReactDOM.render(<Page />, document.getElementById("root"));
 
 ReactDOM.render(<App />, document.getElementById("root"));
